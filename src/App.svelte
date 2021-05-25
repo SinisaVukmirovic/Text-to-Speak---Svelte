3115e325758f87f7aef42da107564ee6437158a4
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2021-05-25 23:23:58 +0200

    Started functionality, getting voices

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+    import { onMount } from 'svelte';
+
+    import {
+        Col,
+        Container,
+        Row,
+        Input,
+        Label,
+        FormGroup,
+        Button,
+    } from "sveltestrap";
+
+    onMount(() => {
+        speechSynthesis.onvoiceschanged = () => {
+            console.log(speechSynthesis.getVoices(), 'voices');
+        }
+    });
+</script>
+
+<style>
+    /* your styles go here */
+</style>
+
+<!-- markup (zero or more items) goes here -->
